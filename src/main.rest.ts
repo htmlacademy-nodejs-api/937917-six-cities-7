12f0715f14afd080bb7d1db5167d76d62b472edb
Yuri Shvetsov <shvetsov.yuri.new@gmail.com>
2024-05-12 16:28:38 +0500

    feat: add dependencies container

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,12 +1,20 @@
-import { PinoLogger } from './shared/libs/logger/index.js';
+import 'reflect-metadata';
+import { Container } from 'inversify';
+
+import { Logger, PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
-import { RestConfig } from './shared/libs/config/index.js';
+import { Config, RestConfig, RestSchema } from './shared/libs/config/index.js';
+import { Component } from './shared/types/index.js';
+
 
 async function bootstrap() {
-  const logger = new PinoLogger();
-  const config = new RestConfig(logger);
+  const container = new Container();
 
-  const application = new RestApplication(logger, config);
+  container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
+  container.bind<Logger>(Component.Logger).to(PinoLogger).inSingletonScope();
+  container.bind<Config<RestSchema>>(Component.Config).to(RestConfig).inSingletonScope();
+
+  const application = container.get<RestApplication>(Component.RestApplication);
 
   await application.init();
 }
