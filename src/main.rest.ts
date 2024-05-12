a86792e63a990ed818e1d87e57a7647664739003
Yuri Shvetsov <shvetsov.yuri.new@gmail.com>
2024-05-12 15:53:04 +0500

    feat: add rest config

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -1,9 +1,12 @@
 import { PinoLogger } from './shared/libs/logger/index.js';
 import { RestApplication } from './rest/index.js';
+import { RestConfig } from './shared/libs/config/index.js';
 
 async function bootstrap() {
   const logger = new PinoLogger();
-  const application = new RestApplication(logger);
+  const config = new RestConfig(logger);
+
+  const application = new RestApplication(logger, config);
 
   await application.init();
 }
